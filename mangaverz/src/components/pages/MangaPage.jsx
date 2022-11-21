1f3e3745a2b8e7f76371d6742458bc139d2643c9
Berat <berat.kamali@student.hogent.be>
2022-11-21 12:02:01 +0100

    Chakraui sidebar

---
+++ mangaverz/src/components/pages/MangaPage.jsx
@@ -0,0 +1,11 @@
+
+import MangaList from "../manga/MangaList"
+import Topbar from "../Topbar"
+export default function MangaPage(){
+  return(
+    <>
+      <Topbar/>
+      <MangaList/>
+    </>
+  )
+}
